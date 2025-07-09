ba29a42a08b7ea11200beb6453c15e7a96af991c
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-07-09 16:00:34 -0300

    Adding error suporting

---
+++ src/errors/HttpError.ts
@@ -0,0 +1,7 @@
+export class HttpError extends Error{
+    status: number
+    constructor(message: string, status: number = 400) {
+        super(message)
+        this.status = status
+    }
+}
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { ErrorRequestHandler } from "express";
+import { HttpError } from "../errors/HttpError";
+
+export const errorHandler: ErrorRequestHandler = async (err, req, res, next)=> {
+    if (err instanceof HttpError) {
+        res.status(err.status).json({message: err.message})
+
+    } else if (err instanceof Error)  {
+        res.status(400  ).json({message: err.message})
+    }else {
+        res.status(500).json({message: err.message})
+    }
+}
